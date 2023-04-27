77de73cbabaa32666f4d98b9bf34221a772bff17
JessePomeroy <thinkingofview@gmail.com>
2023-04-27 12:10:32 -0500

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,86 +47,86 @@ const questionArray =
     }];
 
 //event listeners
-startButton.addEventListener("click", function () {
-    startButton.classList.add("hide");
-    startQuiz();
+startButton.addEventListener("click", function () { // when start button is clicked
+    startButton.classList.add("hide"); // adds class of hide which has a display of none
+    startQuiz(); // run start quiz 
 });
 
-restartButton.addEventListener("click", function () {
-    location.reload();
+restartButton.addEventListener("click", function () { // when reset button is clicked
+    location.reload(); // run start quiz 
 });
 
 //functions
 function startQuiz() {
-    countdown();
-    showQuestion();
+    countdown(); // run countdown
+    showQuestion(); // run show question
 }
-// timer
+
 function countdown() {
     const interval = setInterval(() => {
-        timeLeft--;
-        timer.textContent = "Timer: " + timeLeft + " seconds";
-        if (timeLeft <= 0 || complete) {
-            clearInterval(interval);
-            endQuiz();
+        timeLeft--; // subtract 1 from initial time
+        timer.textContent = "Timer: " + timeLeft + " seconds"; // add text to timer div
+        if (timeLeft <= 0 || complete) { // if timer is 0 or state is complete
+            clearInterval(interval); // stop timer
+            endQuiz(); // run end quiz
         }
-    }, 1000);
+    }, 1000); // 1000 milliseconds
 }
-// populates question and choices
+
 function showQuestion() {
-    answer.innerHTML = '';
-    let totalCount = questionArray.length;
-    let currentQuestion = questionArray[questionCount]
-    questionContent.innerHTML = currentQuestion.question;
-    currentQuestion.choices.forEach(choice => {
-        let option = document.createElement("div");
-        option.innerText = choice;
-        option.className = "choice";
-        option.addEventListener("click", () => {
-            check(choice, currentQuestion.answer, totalCount);
+    answer.innerHTML = ''; // clear answer div
+    let totalCount = questionArray.length; // get total items in array
+    let currentQuestion = questionArray[questionCount] // get current question by looking at array and applying the questionCount
+    questionContent.innerHTML = currentQuestion.question; // add question to questionContent div
+    currentQuestion.choices.forEach(choice => { // get all options for current question
+        let option = document.createElement("div"); // create div for option
+        option.innerText = choice; // add anwser to choice div
+        option.className = "choice"; // add choice class to div
+        option.addEventListener("click", () => {  // add event listener of click to div
+            check(choice, currentQuestion.answer, totalCount); // run check and send the item clicked, the correct answer, and the total questions
         });
-        answer.appendChild(option);
+        answer.appendChild(option); // add div to the answer container
     });
 }
-// check if answer is right or wrong and if wrong deducts 5 seconds from the time/score
+
 function check(choice, answer, totalCount) {
-    if (choice === answer) {
+    if (choice === answer) { // if the anwser chosen is correct
         alert("Good job dawg")
-    } else {
+    } else { // if the anwser chosen is incorrect
+        timeLeft = timeLeft - 5
         alert("Nah, you tripping")
-        timeLeft = timeLeft - 5;
     }
-    questionCount++;
-    if (questionCount === totalCount) {
+    questionCount++; // add 1 to the question count
+    if (questionCount === totalCount) { // if question count and total count equal the same then change the state to complete
         complete = true
-    } else {
+    } else { // if question count and total count does not equal the same the proceed to next question
         showQuestion();
     }
 }
-// anounces end and sets scores to local storage and calls the createScores function
+
 function endQuiz() {
-    quiz.classList.add("hide");
+    quiz.classList.add("hide"); // adds class of hide which has a display of none
     var name = prompt("Quiz is over! Your quiz score is: " + timeLeft + "\nEnter your name to submit your score:");
-    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-    if (name) {
-        highScores.push({ name: name, score: timeLeft });
-        localStorage.setItem("highScores", JSON.stringify(highScores));
-        highScores.sort((a, b) => b.score - a.score);
-        createScores(highScores);
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || []; // get the high scores from local storage
+    if (name) { // if there was input entered in the prompt
+        highScores.push({ name: name, score: timeLeft }); // pushes name and score to high scores array
+        localStorage.setItem("highScores", JSON.stringify(highScores)); // add array to local storage
+        highScores.sort((a, b) => b.score - a.score); // sort by high score
+        createScores(highScores); // send new array to create score function
     } else {
-        endQuiz();
+        endQuiz(); // if no input was entered then re run function
     }
 }
-// populates the scores onto the screen as list elements
+
 function createScores(highScores) {
-    scores.classList.remove("hide");
-    restartButton.classList.remove("hide");
-    highScores.forEach(score => {
-        let playerName = document.createElement("li");
-        let playerScore = document.createElement("li");
-        playerName.innerHTML = score.name;
-        playerScore.innerHTML = score.score;
-        allNames.appendChild(playerName);
-        allScores.appendChild(playerScore);
+    scores.classList.remove("hide"); // removes class of hide 
+    restartButton.classList.remove("hide"); // removes class of hide 
+    highScores.forEach(score => { // get each entry in the high scores array
+        let playerName = document.createElement("li"); // create a list item for name
+        let playerScore = document.createElement("li"); // create a list item for score
+        playerName.innerHTML = score.name; // add name to playerName div
+        playerScore.innerHTML = score.score; // add score to playerName div
+        allNames.appendChild(playerName); // append li to unordered name list
+        allScores.appendChild(playerScore); // append li to unordered score list
     });
 }
